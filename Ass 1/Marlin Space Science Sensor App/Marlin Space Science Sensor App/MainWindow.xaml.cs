ae9a1508340c7e0fafdb3f91baf0a0fab43c2d6a
king_hello <duncannoah341@gmail.com>
2024-01-30 12:58:33 +0800

    30/01/2024
    Finished first two of 15 coding requirements

--- Ass 1/Marlin Space Science Sensor App/Marlin Space Science Sensor App/MainWindow.xaml.cs
+++ Ass 1/Marlin Space Science Sensor App/Marlin Space Science Sensor App/MainWindow.xaml.cs
@@ -8,17 +8,78 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Xceed.Wpf.Toolkit;
+using Galileo6;
 
 namespace Marlin_Space_Science_Sensor_App
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+
+    class displayInfo
+    {
+        public double DisplaySensorA { get; set; }
+        public double DisplaySensorB { get; set; }
+    }
+
     public partial class MainWindow : Window
     {
+        LinkedList<double> sensorA = new LinkedList<double>();
+        LinkedList<double> sensorB = new LinkedList<double>();
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        public void LoadData()
+        {
+            var test = new ReadData();
+            double mu = Convert.ToDouble(Mu.Text);
+            double sigma = Convert.ToDouble(Sigma.Text);
+            var test2 = test.SensorA(mu, sigma);
+            var test3 = test.SensorB(mu, sigma);
+            while (sensorA.Count != 400) 
+            {
+                test2 = test.SensorA(mu, sigma);
+                sensorA.AddFirst(test2);
+            }
+
+            while (sensorB.Count != 400)
+            {
+                test3 = test.SensorA(mu, sigma);
+                sensorB.AddFirst(test3);
+            }
+            Display();
+
+        }
+
+        public void Display()
+        {
+            int index = 0;
+            foreach (double sensor in sensorA)
+            {
+                double sensor2 = sensorB.ElementAt(index);
+                CombinedA_B_Listview.Items.Add(new displayInfo { DisplaySensorA = sensor, DisplaySensorB = sensor2 });
+                index++;
+            }
+
+            foreach (double sensor in sensorA)
+            {
+                SensorA_Listbox.Items.Add(sensor);
+            }
+
+            foreach (double sensor in sensorB)
+            {
+                SensorB_Listbox.Items.Add(sensor);
+            }
+        }
+
+        private void Load_Button(object sender, RoutedEventArgs e)
+        {
+            LoadData();
+        }
     }
+    
 }
--- Ass 1/Marlin Space Science Sensor App/Marlin Space Science Sensor App/obj/Debug/net6.0-windows/Marlin Space Science Sensor App.AssemblyInfo.cs
+++ Ass 1/Marlin Space Science Sensor App/Marlin Space Science Sensor App/obj/Debug/net6.0-windows/Marlin Space Science Sensor App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Marlin Space Science Sensor App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78638920a1d7a4e55719795e6ccf11634622e96b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Marlin Space Science Sensor App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Marlin Space Science Sensor App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
